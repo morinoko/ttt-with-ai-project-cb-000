b8586626693d0ff35ab38bb4d77b80fc54a22d42
Felice Forby <feliceforby@gmail.com>
2018-05-19 11:46:27 +0000

    add Computer Player class with very basic AI

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,3 +1,10 @@
-class Computer < Player
-  
+module Players
+
+  class Computer < Player
+    def move(board)
+      valid_moves = (1..9).to_a.collect{ |value| value.to_s }
+      valid_moves.sample
+    end
+  end
+
 end
