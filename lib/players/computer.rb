b3c5e0a6d1dced1cf673cedb51b931ba330a3124
Felice Forby <feliceforby@gmail.com>
2018-05-20 04:45:01 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -45,7 +45,7 @@ module Players
     end
 
     def ready_to_win?( board )
-      board.win_combos.detect do |combo|
+      WIN_COMBINATIONS.detect do |win_combo|
         win_index_1 = win_combo[0]
         win_index_2 = win_combo[1]
         win_index_3 = win_combo[2]
