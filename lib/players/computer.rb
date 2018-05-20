6474e7c932fb0247d524bccd91875dc688700486
Felice Forby <feliceforby@gmail.com>
2018-05-20 04:44:34 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,6 +3,16 @@ module Players
   class Computer < Player
     POSSIBLE_MOVES = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
     CORNER_MOVES = [ "1", "3", "7", "9" ]
+    WIN_COMBINATIONS = [
+      [0,1,2], # Top row
+      [3,4,5], # Middle row
+      [6,7,8], # Bottom row
+      [0,3,6], # Left column
+      [1,4,7], # Middle column
+      [2,5,8], # Right column
+      [6,4,2], # Diagonal left bottom to right Top
+      [0,4,8]  # Diagonal left top to right bottom
+    ]
 
     def move( board )
       move = nil
