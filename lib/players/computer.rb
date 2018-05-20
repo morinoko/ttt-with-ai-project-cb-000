0fb2a08175bfaca5f501266990a496ed64d76b62
Felice Forby <feliceforby@gmail.com>
2018-05-20 03:58:06 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,9 +1,13 @@
 module Players
 
   class Computer < Player
+    VALID_MOVES = (1..9).to_a.collect { |position| position.to_s }
+    
     def move(board)
-      valid_moves = (1..9).to_a.collect { |position| position.to_s }
+      
+    end
 
+    def random_move
       move = valid_moves.sample
       while board.taken?( move )
         move = valid_moves.sample
